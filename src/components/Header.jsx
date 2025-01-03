21fd5e678c678002a6536b331360763d716abd6b
Afonso Lima <afonsolima@gmail.com>
2025-01-03 16:18:08 -0300

    adjusting header layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <header className={styles.header}>
       <img src={igniteLogo} alt="Logo site" />
-      <strong>Ignite feed</strong>
+      <strong>MBA - Ignite Feed</strong>
     </header>
   );
 }
